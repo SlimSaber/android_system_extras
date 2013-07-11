f63c261b4a890dd5540d9a7ecbea393522b1352d
Amit Pundir <amit.pundir@linaro.org>
2013-07-11 15:11:06 +0100

    update how we run test_system test
    Change-Id: I25e6251f83dd90610f67408d1c9522b16fa380dd
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- tests/bionic/libc/run-test_linaro.sh
+++ tests/bionic/libc/run-test_linaro.sh
@@ -14,7 +14,7 @@
 #     Test sources are available at "system/extras/tests/bionic/libc" 
 #############################################################################
 
-TESTS="bench_locks bench_pthread_shared bench_pthread_static bench_stdio test_aligned test_arc4random test_atomics test_clock test_clone test_cpu_set test_dlclose_destruction test_dlopen_null test_drand48 test_executable_destructor test_getaddrinfo test_getgrouplist test_gethostbyname test_gethostname test_libgen test_mutex test_netinet_icmp test_pthread_cleanup_push test_pthread_cond test_pthread_create test_pthread_getcpuclockid test_pthread_join test_pthread_mutex test_pthread_once test_pthread_rwlock test_relocs test_semaphore test_sem_post test_setjmp test_seteuid test_static_cpp_mutex test_static_executable_destructor test_static_init test_strftime_2039 test_strptime test_sysconf test_system test_timer_create3 test_tm_zone test_udp test_vfprintf_leak"
+TESTS="bench_locks bench_pthread_shared bench_pthread_static bench_stdio test_aligned test_arc4random test_atomics test_clock test_clone test_cpu_set test_dlclose_destruction test_dlopen_null test_drand48 test_executable_destructor test_getaddrinfo test_getgrouplist test_gethostbyname test_gethostname test_libgen test_mutex test_netinet_icmp test_pthread_cleanup_push test_pthread_cond test_pthread_create test_pthread_getcpuclockid test_pthread_join test_pthread_mutex test_pthread_once test_pthread_rwlock test_relocs test_semaphore test_sem_post test_setjmp test_seteuid test_static_cpp_mutex test_static_executable_destructor test_static_init test_strftime_2039 test_strptime test_sysconf test_timer_create3 test_tm_zone test_udp test_vfprintf_leak"
 
 # Rogue tests
 #TESTS+= "test_cond test_thread_max test_timer_create test_timer_create2"
@@ -28,3 +28,11 @@ for TEST in $TESTS; do
 		echo "$TEST : PASS"
 	fi
 done
+
+test_system date
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -ne 0 ]; then
+	echo "$TEST : FAIL"
+else
+	echo "$TEST : PASS"
+fi
